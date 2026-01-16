6e8ec04e4c2ce240c19c3ce70d451d5ad3c6b49e
Steve Copley <steve.copley@waimea.school.nz>
2026-01-17 00:14:22 +1300

    Rules and actions setup

--- sand.js
+++ sand.js
@@ -1,15 +1,12 @@
-const canvas = document.getElementById("canvas")
-const ctx = canvas.getContext('2d');
-
 const SCALE  = 5
-const WIDTH  = Math.floor(1000 / SCALE)
+const WIDTH  = Math.floor(800 / SCALE)
 const HEIGHT = Math.floor(600 / SCALE)
 
-const sand = []
+const targetFPS = 30;
+const frameDelay = 1000 / targetFPS;
 
-const EMPTY = 0
-const SOLID = [255, 255, 255]
-
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext('2d');
 let imageData
 let imageDataArray
 
@@ -19,10 +16,7 @@ let isLeftMouseDown = false
 let isRightMouseDown = false
 let isShiftDown = false
 
-const targetFPS = 30;
-const frameDelay = 1000 / targetFPS;
-
-let hue = 0
+let hue = 40
 
 const brush = [
                         [-1, -3], [ 0, -3], [ 1, -3],
@@ -33,6 +27,127 @@ const brush = [
               [-2,  2], [-1,  2], [ 0,  2], [ 1,  2], [ 2,  2],
                         [-1,  3], [ 0,  3], [ 1,  3]
 ]
+
+
+const grid = []
+
+const ANY   = " "
+const EMPTY = "E"
+const WALL  = "W"
+const SAND  = "S"
+
+const ANY_ROW = "   "
+const MATCH_SIZE = 3
+const MATCH_OFFSET = Math.floor(MATCH_SIZE / 2)
+const MATCH_ALL = MATCH_SIZE * MATCH_SIZE
+
+console.log(MATCH_SIZE, MATCH_OFFSET, MATCH_ALL)
+
+class Particle {
+    constructor(col, rules) {
+        this.col = col
+        this.rules = rules
+    }
+
+    update(cell) {
+        this.rules.forEach(rule => {
+            const didUpdate = rule.update(cell)
+            if (didUpdate)
+                return
+        })
+    }
+}
+
+class Rule {
+    constructor(match, action, canMirror) {
+        this.match = match
+        this.action = action
+        this.canMirror = canMirror
+    }
+
+    update(cell) {
+        const reverseMatch = this.canMirror & Math.random() < 0.5
+
+        let matches = 0
+        let yOff = WIDTH * -MATCH_OFFSET
+        this.match.forEach(matchRow => {
+            if (matchRow === ANY_ROW) {
+                matches += 3
+            }
+            else {
+                for(let i = 0; i < MATCH_SIZE; i++) {
+                    const matchIndex = reverseMatch ? MATCH_SIZE - i - 1 : i
+                    const matchCode = matchRow[matchIndex]
+                    if (matchCode === ANY || matchCode === grid[cell + yOff - MATCH_OFFSET + i]) {
+                        matches++
+                    }
+                }
+            }
+            yOff += WIDTH
+        })
+
+        if (matches !== MATCH_ALL)
+            return false
+
+        yOff = WIDTH * -MATCH_OFFSET
+        this.action.forEach(actionRow => {
+            for(let i = 0; i < MATCH_SIZE; i++) {
+                const actionIndex = reverseMatch ? MATCH_SIZE - i - 1 : i
+                const code = actionRow[actionIndex]
+                if (code !== ANY) {
+                    grid[cell + yOff - MATCH_OFFSET + i] = code
+                }
+            }
+            yOff += WIDTH
+        })
+
+        return true
+    }
+}
+
+const particles = {
+    "E": new Particle(
+        [0, 0, 0],
+        []
+    ),
+
+    "W": new Particle(
+        [255, 255, 255],
+        []
+    ),
+
+    "S": new Particle(
+        [255, 255, 0],
+        [
+            new Rule(
+                [
+                    "   ",
+                    " S ",
+                    " E "
+                ],
+                [
+                    "   ",
+                    " E ",
+                    " S "
+                ],
+                false
+            ),
+            new Rule(
+                [
+                    "   ",
+                    " S ",
+                    "  E"
+                ],
+                [
+                    "   ",
+                    " E ",
+                    "  S"
+                ],
+                true
+            ),
+        ]
+    )
+}
 
 const hslToRgb = (h, s, l) => {
     // h: 0-360, s: 0-100, l: 0-100
@@ -115,7 +230,7 @@ const initCanvas = () => {
 
         if (e.code === 'Space') {
             e.preventDefault()
-            clearSand()
+            clearGrid()
         }
     })
 
@@ -126,47 +241,22 @@ const initCanvas = () => {
     })
 }
 
-
-class Action {
-    constructor(mapping, action) {
-        this.mapping = mapping
-        this.action = action
-    }
-}
-
-class Particle {
-    constructor(type, col, actions) {
-        this.type = type
-        this.col = col
-        this.actions = actions
-    }
-
-    update(grid) {
-        this.actions.forEach(action => {
-
-        })
-    }
-}
-
-
-
-
-const initSand = () => {
+const initGrid = () => {
     const cells = WIDTH * HEIGHT
 
     for(let cell = 0; cell < cells; cell++) {
-        sand[cell] = EMPTY
+        grid[cell] = EMPTY
     }
 }
 
-const clearSand = () => {
-    for(let cell = 0; cell < sand.length; cell++) {
-        if (!isShiftDown && sand[cell] !== SOLID) {
-            sand[cell] = EMPTY
+const clearGrid = () => {
+    for(let cell = 0; cell < grid.length; cell++) {
+        if (!isShiftDown && grid[cell] !== WALL) {
+            grid[cell] = EMPTY
             continue
         }
-        if (isShiftDown && sand[cell] === SOLID) {
-            sand[cell] = EMPTY
+        if (isShiftDown && grid[cell] === WALL) {
+            grid[cell] = EMPTY
             continue
         }
     }
@@ -176,31 +266,24 @@ const xyCell = (x, y) => {
     return (y * WIDTH) + x
 }
 
-const swapSand = (cell1, cell2) => {
-    const val1 = sand[cell1]
-    const val2 = sand[cell2]
-    sand[cell1] = val2
-    sand[cell2] = val1
+const swapCells = (cell1, cell2) => {
+    const val1 = grid[cell1]
+    const val2 = grid[cell2]
+    grid[cell1] = val2
+    grid[cell2] = val1
 }
 
 const checkMouse = () => {
     if (isLeftMouseDown) {
         if (isShiftDown) {
-            const cell = xyCell(mouseX, mouseY)
-            sand[cell] = SOLID
+            grid[xyCell(mouseX, mouseY)] = WALL
             return
         }
 
         brush.forEach(([x, y]) => {
             const cell = xyCell(mouseX + x, mouseY + y)
-
-            if (sand[cell] !== SOLID && Math.random() > 0.7) {
-                const light = Math.floor(40 + Math.random() * 10)
-                sand[cell] = hslToRgb(hue, 50, light)
-            }
-
-            if (Math.random() > 0.99) {
-                hue = (hue + 1) % 360
+            if (grid[cell] !== WALL && Math.random() > 0.7) {
+                grid[cell] = SAND
             }
         })
         return
@@ -208,71 +291,26 @@ const checkMouse = () => {
 
     if (isRightMouseDown) {
         brush.forEach(([x, y]) => {
-            sand[xyCell(mouseX + x, mouseY + y)] = EMPTY
+            grid[xyCell(mouseX + x, mouseY + y)] = EMPTY
         })
     }
 }
 
-let scanDirection = 1
-const updateSand = () => {
-    for (let y = HEIGHT - 1; y >= 0; y--) {
-        scanDirection *= -1
+const updateGrid = () => {
+    const updateDirection = Math.random() < 0.5 ? WIDTH : 0
 
-        const start = scanDirection > 0 ? 0 : WIDTH - 1
-        const end = scanDirection > 0 ? WIDTH : -1
-
-        for (let x = start; x !== end; x += scanDirection) {
-            const cell = xyCell(x, y)
-            const cellBelow = cell + WIDTH
-
-            if (sand[cell] === EMPTY || sand[cell] === SOLID) continue
-            // if (sand[cellBelow] === SOLID) continue
-
-            if (sand[cellBelow] === EMPTY) {
-                swapSand(cell, cellBelow)
-                continue
-            }
-
-            const tryLeftFirst = Math.random() < 0.5
-            const diagonals = tryLeftFirst
-                ? [cellBelow - 1, cellBelow + 1, cellBelow - 2, cellBelow + 2]
-                : [cellBelow + 1, cellBelow - 1, cellBelow + 2, cellBelow - 2]
-            const sides = tryLeftFirst
-                ? [cell - 1, cell + 1, cell - 2, cell + 2]
-                : [cell + 1, cell - 1, cell + 2, cell - 2]
-
-            let swapped = false
-            for (const diagonal of diagonals) {
-                if (sand[diagonal] === EMPTY) {
-                    swapSand(cell, diagonal)
-                    swapped = true
-                    break
-                }
-            }
-
-            // if (!swapped) {
-            //     for (const side of sides) {
-            //         if (sand[side] === EMPTY) {
-            //             swapSand(cell, side)
-            //             break
-            //         }
-            //     }
-            // }
-        }
+    for (let cell = grid.length - 1; cell >= 0; cell--) {
+        const particleType = grid[cell]
+        const particle = particles[particleType]
+        particle.update(cell)
     }
 }
 
-const drawSand = () => {
-    for (let i = 0; i < sand.length; i++) {
-        const imageIndex = i * 4
-
-        if (sand[i]) {
-            [r, g, b] = sand[i]
-        }
-        else {
-            [r, g, b] = [0, 0, 0]
-        }
-
+const drawGrid = () => {
+    for (let cell = 0; cell < grid.length; cell++) {
+        const particle = particles[grid[cell]]
+        const [r, g, b] = particle.col
+        const imageIndex = cell * 4
         imageDataArray[imageIndex + 0] = r
         imageDataArray[imageIndex + 1] = g
         imageDataArray[imageIndex + 2] = b
@@ -290,10 +328,10 @@ const drawSand = () => {
 let lastTime = 0;
 animate = (currentTime) => {
     checkMouse()
-    updateSand()
+    updateGrid()
 
     if (currentTime - lastTime >= frameDelay) {
-        drawSand()
+        drawGrid()
         lastTime = currentTime;
     }
 
@@ -302,5 +340,5 @@ animate = (currentTime) => {
 
 
 initCanvas()
-initSand()
+initGrid()
 animate()
